f0138743dc23168e9a4ae61550312dfb37641458
minikiller@qq.com <minikiller@qq.com>
2018-04-28 10:40:17 +0800

    change build.sh

--- karaf/build.sh
+++ karaf/build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker build openshift/s2i-karaf .
+docker login -u admin -p $(oc whoami -t)  docker-registry.default.svc:5000; \
+docker build -t docker-registry.default.svc:5000/openshift/s2i-karaf . ; \
+docker push docker-registry.default.svc:5000/openshift/s2i-karaf
